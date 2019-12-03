a7d8d61ef25b5b49aa27cba39432053b5c1d0fde
fickrybiliman <bil.iman.fickry@gmail.com>
2019-12-03 19:50:12 +0700

    kasus penjumlahan matriks, percobaan 1

---
+++ novice/01-01/Pertama.java
@@ -0,0 +1,9 @@
+public class Pertama {
+   public static void main(String [] arguments) {
+      int x = 10;
+      System.out.println("Hello World!");
+      System.out.println(x);
+      x += 11;
+      System.out.println(x);
+   }
+}
