b2f8decb92f9686f53a49dc9d6c74941d7bb56f4
fickrybiliman <bil.iman.fickry@gmail.com>
2019-12-04 10:55:47 +0700

    input penjumlahan matriks

---
+++ novice/01-01/kasus/inputPenjumlahanMatriks.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class inputPenjumlahanMatriks {
+   public static void main(String[] arguments) {
+      Scanner input = new Scanner(System.in);
+      
+      System.out.println("Masukkan Tinggi Matriks : ");
+      int heightMatriks = input.nextInt();
+
+      System.out.println("Masukkan Lebar Matriks : ");
+      int widthMatriks = input.nextInt();
+
+      System.out.println("Tinggi Matriks : " + heightMatriks);
+      System.out.println("Lebar Matriks : " + widthMatriks);
+
+      for (int i = 0; i < heightMatriks; i++) {
+         for (int j = 0; j < widthMatriks; j++) {
+
+         }
+      }
+   }
+}
--- novice/01-01/kasus/penjumlahanMatriks.java
+++ novice/01-01/kasus/penjumlahanMatriks.java
@@ -2,33 +2,20 @@ import java.util.Arrays;
 
 public class penjumlahanMatriks {
    public static void main(String[] arguments) {
-      int satu[] = { 1, 2, 3,
-                     4, 5, 6,
-                     7, 8, 9
-                   };
-      
-      int dua[] = { 10, 11, 12, 
-                    13, 14, 15,
-                    16, 17, 18
-                  };
-      
-      int tiga[] = { 100, 200, 300,
-                     400, 500, 600,
-                     700, 800, 900
-                   };
-      
-      System.out.println(tiga);
+      int satu[][] = { {1, 2, 3}, 
+                       {4, 5, 6}, 
+                       {7, 8, 9} 
+                     };
+      int dua[][] = { {11, 22, 33}, 
+                      {44, 55, 66}, 
+                      {77, 88, 99} 
+                    };
 
-      int hasil[] = new int[9];
-      
-      // for (int i = 0; i < 9; i++) {
-      //    System.out.println(satu[i]);
-      //    System.out.println(dua[i]);
-      //    System.out.println(tiga[i]);
-      //    hasil[i] = satu[i] + dua[i] + tiga[i];
-      //    System.out.println(hasil[i]);
-      // }
-
-      // System.out.println(hasil);
+      for (int i = 0; i < satu.length; i++) {
+         for (int j = 0; j < satu[i].length; j++) {
+            System.out.print(satu[i][j] + dua[i][j] + " ");
+         }
+         System.out.println();
+      }
    }
 }
