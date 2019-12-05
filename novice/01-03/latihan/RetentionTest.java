62e166051212a8dea36aee9fce6cd714d0bba0ab
fickrybiliman <bil.iman.fickry@gmail.com>
2019-12-05 16:15:19 +0700

    @retention annotations test

--- novice/01-03/latihan/RetentionTest.java
+++ novice/01-03/latihan/RetentionTest.java
@@ -10,5 +10,20 @@ import java.lang.annotation.RetentionPolicy;
 }
 
 public class RetentionTest {
-   
+   @TestAnnotation(Developer = "data", Expirydate = "01-10-2020")
+
+   void fun1() {
+      System.out.println("Test Method 1");
+   }
+
+   @TestAnnotation(Developer = "fair", Expirydate = "01-10-2020")
+
+   void fun2() {
+      System.out.println("Test Method 2");
+   }
+
+   public static void main(String args[]) {
+      System.out.println("Hello");
+   }
+
 }
