281d012610b2c0afd35b8604984593dd1577348a
fickrybiliman <bil.iman.fickry@gmail.com>
2019-12-13 08:43:38 +0700

    fix app.java on myrunnable

--- novice/02-03/latihan/MyRunnable/src/main/java/MyRunnable/App.java
+++ novice/02-03/latihan/MyRunnable/src/main/java/MyRunnable/App.java
@@ -4,6 +4,7 @@
 package MyRunnable;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class App {
     public String getGreeting() {
@@ -18,6 +19,23 @@ public class App {
         for (int i = 0; i < 500; i++) {
             Runnable task = new MyRunnable(10000000L + i);
             Thread worker = new Thread(task);
+            // We can set the name of the thread
+            worker.setName(String.valueOf(i));
+            // Start the thread, never call method run() direct
+            worker.start();
+            // Remember the thread for later usage
+            threads.add(worker);
         }
+
+        int running = 0;
+        do {
+            running = 0;
+            for (Thread thread : threads) {
+                if (thread.isAlive()) {
+                    running++;
+                }
+            }
+            System.out.println("We have " + running + " running threads.");
+        } while (running > 0);
     }
 }
